779663e347249cf2daec676ed8d8e1ebe050cf75
zartaz <kzartas@gmail.com>
2022-10-21 23:25:38 +0300

    final for today

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ const config = {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
 };
-console.log('config', config);
-// exit(0);
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
