f1a3acf555a5e9d24a7f0de9a2e9bee8db92739e
zartaz <kzartas@gmail.com>
2022-10-23 22:43:34 +0300

    RateLimitPassAttempts

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
 require('dotenv').config();
+const rateLimit = require('express-rate-limit');
 const express = require('express');
 const ParseServer = require('parse-server').ParseServer;
 const ParseDashboard = require('parse-dashboard');
 const path = require('path');
 const args = process.argv || [];
 const test = args.some(arg => arg.includes('jasmine'));
-
 const databaseUri = process.env.DB_URI;
-
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
@@ -21,7 +18,7 @@ const config = {
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
-  },
+  }
 };
 const options = {
   allowInsecureHTTP: true,
@@ -34,19 +31,30 @@ const parseConfig = {
       "masterKey": process.env.MASTER_KEY,
       "appName": process.env.APP_NAME
     }
+  ],
+  "users": [
+    {
+      "user": process.env.APP_USER,
+      "pass": process.env.APP_PASS
+    }
   ]
+
 };
-
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
-
 const app = express();
+const sendMessageRateLimiterMiddleware = rateLimit({
+  windowsMs: 60 * 1000 * 15,
+  max: 5,
+  message: 'You have exceeded 5 false password attempts, please try again after 15 minutes',
+  standardHeaders: true,
+  legacyHeaders: false,
+  skipSuccessfulRequests: true
+});
 
-// Serve static assets from the /public folder
+
 app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use('/parse/functions/login', sendMessageRateLimiterMiddleware);
 
-// Serve the Parse API on the /parse URL prefix
+
 const mountPath = process.env.PARSE_MOUNT || '/parse';
 if (!test) {
   const api = new ParseServer(config);
@@ -55,13 +63,12 @@ if (!test) {
   app.use('/dashboard', dashboard);
 }
 
-// Parse Server plays nicely with the rest of your web routes
+
 app.get('/', function (req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
+
 app.get('/test', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
